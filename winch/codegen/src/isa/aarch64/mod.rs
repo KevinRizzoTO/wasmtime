e8da16c0b6c256abbde051216c6e1cfc064a5860
Alex Crichton <alex@alexcrichton.com>
2023-02-13 18:34:07 -0800

    Implement `Compiler::compile_function` for Winch
    Hook this into the `TargetIsa::compile_function` hook as well. Currently
    this doesn't take into account `Tunables`, but that's left as a TODO for
    later.

--- winch/codegen/src/isa/aarch64/mod.rs
+++ winch/codegen/src/isa/aarch64/mod.rs
@@ -83,14 +83,14 @@ impl TargetIsa for Aarch64 {
         &self,
         sig: &FuncType,
         body: &FunctionBody,
-        mut validator: FuncValidator<ValidatorResources>,
+        validator: &mut FuncValidator<ValidatorResources>,
     ) -> Result<MachBufferFinalized<Final>> {
         let mut body = body.get_binary_reader();
         let mut masm = Aarch64Masm::new(self.shared_flags.clone());
         let stack = Stack::new();
         let abi = abi::Aarch64ABI::default();
         let abi_sig = abi.sig(sig);
-        let frame = Frame::new(&abi_sig, &mut body, &mut validator, &abi)?;
+        let frame = Frame::new(&abi_sig, &mut body, validator, &abi)?;
         // TODO: Add floating point bitmask
         let regalloc = RegAlloc::new(RegSet::new(ALL_GPR, 0), scratch());
         let codegen_context = CodeGenContext::new(regalloc, stack, &frame);
